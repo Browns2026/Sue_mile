0f735a227a17eda876d6cef1b372eba269934aa7
Sue_mile <browns2026@naver.com>
2025-01-22 23:06:45 -0800

    Update operator_arithmetic.cpp

--- operator_arithmetic.cpp
+++ operator_arithmetic.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include <iostram>
+using namespace std;
+
 int main () {
-  int a = 10, b = 20;
-int sum = a + b; 
-return 0;
+  int a = 15, b = 4;
+
+  cout << "a + b = " << (a + b) << endl; // plus
+  cout << "a - b = " << (a - b) << endl; // minus
+  cout << "a * b = " << (a * b) << endl; // multiple
+  cout << "a / b = " << (a / b) << endl; // division
+  cout << "a % b = " << (a % b) << endl; // rest
+
+  return 0;
 }
